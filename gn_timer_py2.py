ce23bd95757f8c704339a906bfa5817052576551
grrttgrrtt <47403031+grrttgrrtt@users.noreply.github.com>
2019-05-08 05:03:04 -0400

    Add files via upload

--- gn_timer_py2.py
+++ gn_timer_py2.py
@@ -4,36 +4,18 @@ the class level, so multiple generated timer objects will all increment the same
 shared time values. Use it to keep track of how much total time different
 function calls are taking.
 
-The Timer class stores running timers in seconds as attributes. The
-attributes can have long names, but, for convenience, you can abbreviate
-them when writing yor wrappers.
+The Timer class stores running timers in seconds as attributes. Attributes
+correspond to the name of the function which is wrapped.
 
-Timer attributes are connected to their abbreviations by the dictionary
-timer_dict. You can pre-initialize this dict by using Timer.define_timers().
-
-By default, passing an abbreviation not in timer_dict will create a new timer
-with the attribute having the same name as the abbreviation. This behavior
-can be disabled by calling Timer.restrict_abbrs() (useful if you make a typo
-in an abbr and want your code to throw an error instead of creating a
-new timer.
-
-Different timer attributes can be pre-initialized with Timer.define_timers(),
-or generated dynamically.
-
-Wrap functions you want to time with @Timer._time('abbr'). This wrapper
-increments the timer which corresponds to the abbreviation 'abbr'.
+Wrap functions you want to time with @Timer.timer. This wrapper
+increments the timer which corresponds to the name of the function which it
+wraps.
 
 METHODS:
-Timer.define_timers(timer_dict: dict) -> None:
-    Pre-initializes takes a dictionary of abbreviation: attribute pairs.
 
 Timer.timer(timer_key: str) -> Callable:
     Wrapper function to time other functions. Time in seconds of execution is
     stored in the class attribute indicated by timer_key.
-
-Timer.restrict_abbrs() -> None:
-    Disables dynamically creating attributes and requires all timers to be
-    pre-initialized using define_timers().
 
 print_times(): prints all timer attributes
 
@@ -43,7 +25,8 @@ CHANGELOG:
 4-24-19: Branched from gn_timer.py to remove type hints and be python 2
     compatible.
 4-30-19: Updated to prevent dynamic timer creation overwriting class functions
-5-8-19: Updated wrapper to pass functions names through; useful when
+5-02-19: Overhauled. Timer attributes now determined solely by function names.
+5-08-19: Updated wrapper to pass functions names through; useful when
     double-wrapping
 """
 import time as time_module
